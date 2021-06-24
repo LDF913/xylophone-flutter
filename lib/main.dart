25443f43466f36f5b055056f8663a4d25ef8b858
LDF913 <ldf913@gmail.com>
2021-06-23 23:10:11 -0400

    input function variables dart funtions part 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,77 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+
+  void playsound(int soundnumber) {
+    final player = AudioCache();
+    player.play(
+        'note$soundnumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(
-           child: FlatButton(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              FlatButton(
 
-              child: Text('Click Me'),
-            ),
+                color: Colors.red,
+                onPressed: () {
+                playsound(1);
+                },
+                child: Text('click Me'),
+              ),
+              FlatButton(
+                color: Colors.orangeAccent,
+                onPressed: () { playsound(2);
+                },
+                child: Text('click Me'),
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  playsound(3);
+
+                },
+                child: Text('click Me'),
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  playsound(4);
+
+                },
+                child: Text('click Me'),
+              ),
+              FlatButton(
+                color: Colors.lightGreen,
+                onPressed: () { playsound(5);
+                },
+                child: Text('click Me'),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  playsound(6);
+                },
+                child: Text('click Me'),
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () { playsound(7);
+                },
+                child: Text('click Me'),
+              ),
+            ],
           ),
         ),
       ),
